faeb3d4da9f23a87bfe5732bcd639195e52fd74e
iikier <ivanikier@hotmail.com>
2025-06-12 17:30:41 -0300

    feat: implementa Todo List com múltiplos arquivos para ofuscação

--- src/app.js
+++ src/app.js
@@ -1,35 +1,5 @@
-// A simple function to demonstrate obfuscation
-function greet() {
-    const message = "Hello, this is a secret message!";
-    console.log(message);
-    alert(message);
-}
+import './ui.js';
 
-// A class to demonstrate more complex obfuscation
-class Calculator {
-    constructor() {
-        this.result = 0;
-    }
-
-    add(a, b) {
-        this.result = a + b;
-        return this.result;
-    }
-
-    subtract(a, b) {
-        this.result = a - b;
-        return this.result;
-    }
-
-    showResult() {
-        console.log(`The result is: ${this.result}`);
-    }
-}
-
-greet();
-
-const calc = new Calculator();
-calc.add(10, 5);
-calc.showResult();
-calc.subtract(10, 3);
-calc.showResult(); 
+// O arquivo ui.js já inicializa a aplicação
+// Este arquivo serve apenas como ponto de entrada
+console.log('Aplicação Todo List iniciada!'); 
